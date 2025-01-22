6488bc06d50e0099057985f71ad7110c9f1de529
aman0-007 <dwivedi.aman2811@gmail.com>
2025-01-22 13:06:28 +0530

    SplashScreen Started

---
+++ lib/screens/splash/splashscreen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:jito_visuals/screens/users/ADMIN/admin_home.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const AdminHomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/logo/jito.jpg"),
+            Text(""),
+          ],
+        ),
+      ),
+    );
+  }
+}
